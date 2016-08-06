313517c2a8e22b111971c55b7827ef66f9852355
dhalima3 <dhalima@gatech.edu>
2016-08-06 03:03:57 -0400

    feature- add toolbar and compose menu item

--- app/src/main/java/com/codepath/apps/twitterclient/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/twitterclient/TimelineActivity.java
@@ -2,7 +2,10 @@ package com.codepath.apps.twitterclient;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.codepath.apps.twitterclient.adapters.TweetsArrayAdapter;
@@ -27,11 +30,24 @@ public class TimelineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         lvTweets = (ListView) findViewById(R.id.lvTweets);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarTimeline);
+        setSupportActionBar(toolbar);
+
         tweets = new ArrayList<>();
         tweetsArrayAdapter = new TweetsArrayAdapter(this, tweets);
         lvTweets.setAdapter(tweetsArrayAdapter);
         client = TwitterApplication.getRestClient();
         populateTimeline();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+        return true;
+    }
+
+    public void onComposeAction(MenuItem item) {
+
     }
 
     private void populateTimeline() {
